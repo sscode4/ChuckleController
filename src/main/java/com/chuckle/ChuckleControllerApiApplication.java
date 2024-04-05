50327b1c8ca55b1f7a974256db9603e48db12912
SaundaryaSrivastav <srivastavasaundarya8@gmail.com>
2024-04-05 16:48:40 +0530

    01- Chuckle Controller Project setup

---
+++ src/main/java/com/chuckle/ChuckleControllerApiApplication.java
@@ -0,0 +1,13 @@
+package com.chuckle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckleControllerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckleControllerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chuckle/ChuckleControllerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuckle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckleControllerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
