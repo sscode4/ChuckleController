8259618bf5b2821f2daec9d45dbed59d58acb833
SaundaryaSrivastav <srivastavasaundarya8@gmail.com>
2024-04-07 20:21:00 +0530

    01- Chuckle Controller Login ,Resister,Jwt,User and many more features

---
+++ src/main/java/com/chuckle/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.chuckle.repository;
+
+import com.chuckle.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserByUsername(String username);
+
+    User findUserByEmail(String email);
+}
